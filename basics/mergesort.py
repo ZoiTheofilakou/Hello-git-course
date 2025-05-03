23dee0200ed44946e82c8688474764fa44e07029
Zoi Theofilakou <zoi.theofilakou@edu.turkuamk.fi>
2025-05-03 15:52:24 +0300

    Restore mergesort.py from course materials

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,24 +1,52 @@
-input_str = input("Enter numbers, separated by ',': ")
-input_list = input_str.split(",")
-value_list = [int(x.strip()) for x in input_list]
 
-print("input_list:", input_list)
-print("value_list:", value_list)
-print("array:", value_list)
+def debug_print(debug_msg=None, **kwargs):
 
-def merge_sort(array):
-    if len(array) < 2:
+    if debug_msg:
+        print(debug_msg)
+
+    for key, value in kwargs.items():
+        print("{}: {}".format(key, value))
+
+
+def mergesort(array):
+    if len(array) <= 1:
         return array
+
     m = len(array) // 2
-    return merge(merge_sort(array[:m]), merge_sort(array[m:]))
+
+    left = mergesort(array[:m])
+    right = mergesort(array[m:])
+
+    return merge(left, right)
+
 
 def merge(left, right):
-    result = []
-    while left and right:
-        result.append((left if left[0] <= right[0] else right).pop(0))
-    result.extend(left or right)
-    return result
+    merged = []
 
-sorted_list = merge_sort(value_list)
-print(sorted_list)
+    while len(left) > 0 and len(right) > 0:
+        if left[0] <= right[0]:
+            merged.append(left.pop(0))
+        else:
+            merged.append(right.pop(0))
 
+    if len(left) > 0:
+        merged += left
+    else:
+        merged += right
+
+    return merged
+
+
+if __name__ == "__main__":
+    input_str = input("Enter numbers, separated by ',': ")
+    input_list = input_str.split(",")
+    value_list = []
+    for x in input_list:
+        try:
+            value_list.append(int(x))
+        except ValueError as err:
+            print("Invalid input.")
+            quit(1)
+
+    sorted_list = mergesort(value_list)
+    print(sorted_list)
