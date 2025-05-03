ec28e3b90d613f257013ba59131eebec7c7d9036
Zoi Theofilakou <zoi.theofilakou@edu.turkuamk.fi>
2025-05-03 16:14:10 +0300

    final fix

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,8 +1,9 @@
 def debug_print(debug_msg=None, **kwargs):
-    if debug_msg and not kwargs:
+    if debug_msg:
         print(debug_msg)
     for key, value in kwargs.items():
-        print(f"{key}: {value}")
+        print("{}: {}".format(key, value))
+
 
 def mergesort(array):
     debug_print(array=array)
@@ -22,8 +23,10 @@ def mergesort(array):
 
     return merged
 
+
 def merge(left, right):
     merged = []
+
     while len(left) > 0 and len(right) > 0:
         if left[0] <= right[0]:
             merged.append(left.pop(0))
@@ -37,10 +40,11 @@ def merge(left, right):
 
     return merged
 
+
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
     input_list = input_str.split(",")
-    debug_print(input_list=input_list)
+    debug_print(input_list=input_list)  # DO NOT REMOVE THIS
 
     value_list = []
     for x in input_list:
@@ -51,5 +55,7 @@ if __name__ == "__main__":
             quit(1)
 
     debug_print(value_list=value_list)
+
     sorted_list = mergesort(value_list)
     print(sorted_list)
+
